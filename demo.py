eafe214078660c51e4db7f4398df0fcdb19e9efa
Chandra Shekhar Deonath <csdeonath@gmail.com>
2022-05-28 06:53:25 +0530

    added a comment and a print statement

--- demo.py
+++ demo.py
@@ -1,1 +1,4 @@
 print("Hello I am using github that is linked with my pycharm for demo version")
+# I have added a comment and added a new print statement
+print("this is a small change from my own machine")
+
